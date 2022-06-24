53936e9ad074a5eebbbe922fbc632e18c3a77d7e
Francisco Oteo <panchyn.oteo@gmail.com>
2022-06-23 22:42:19 -0300

    Clase 2 - ciclos e iteraciones

---
+++ clase 2/index.js
@@ -0,0 +1,4 @@
+for (let i=1; i<=5; i++) {
+    let nombrePersonal = prompt("Nombre Persona:");
+    alert("Turno para Dentista dia lunes Nº: " + i +  "Nombre: " + nombrePersonal);
+}
---
+++ clase1/index.js
@@ -0,0 +1,55 @@
+/*console.log('hola a todos');*/
+/*
+//variables (espacio en memoria pequeña) y valores - recomendadas
+let numero = 1234;
+const NUMERO = 1;
+
+//foma de variable no recomendada
+var nombre = "francisco";
+
+console.log(typeof(numero));
+
+*/
+
+
+/*number 
+
+let number1 = 4;
+let number2 = 5;
+let resultadoSuma = number1 + number2;
+let resultadoResta = number1 - number2;
+let resultadoMultiplicacion = number1 * number2;
+let resultadoDivision = number1 / number2;
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(resultadoDivision); 
+
+*/
+
+
+/* let text1 = 'Francisco';
+let text2 = 'Agustina';
+console.log(text1 + " " + text2); */
+
+/* let nombre = prompt("Ingrese el nombre de la persona");
+console.log(typeof(nombre)); */
+
+/* let munero1 = parseInt(prompt("ingrese el numero 1"));
+let munero2 = parseInt(prompt("ingrese el numero 2"));
+let resultadoSuma = numero1 + numero2;
+console.log(numero1 + numero2); */
+
+/* let entrada = prompt("Ingresar una letra");
+let salida = entrada + " " + "ingresada";
+alert(salida); */
+
+/* let nombre = prompt("hombre de la persona");
+let apellido = prompt("apellido de la persona");
+alert(nombre + " " + apellido + " " + "bienvenido"); */
+
+let nombre = prompt("hombre de la persona");
+let apellido = prompt("apellido de la persona");
+let edad = prompt("Ingrese edad de la persona");
+console.log(`hola ${nombre} ${apellido} tu edad es ${edad}`);
